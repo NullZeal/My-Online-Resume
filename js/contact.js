12c6cee803b204a89986db1a617ae7ffc2914c19
NullZeal <nullzeal@pm.me>
2022-04-26 21:15:38 -0400

    Added a contact page

---
+++ js/contact.js
@@ -0,0 +1,17 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 45.491657766855326, lng: -73.58155140432127 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 12,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
